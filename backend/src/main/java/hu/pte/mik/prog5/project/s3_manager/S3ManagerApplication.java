903c50be7b221b4762e64f64d68b78a4ce1b2da8
adamkoro <adam.koronics@gmail.com>
2024-12-21 20:03:39 +0100

    init

---
+++ backend/src/main/java/hu/pte/mik/prog5/project/s3_manager/S3ManagerApplication.java
@@ -0,0 +1,13 @@
+package hu.pte.mik.prog5.project.s3_manager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class S3ManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(S3ManagerApplication.class, args);
+	}
+
+}
