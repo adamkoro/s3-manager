d1fe901b60c88d34c3e4ee70a61100647a153384
Adamkoro <adam.koronics@gmail.com>
2025-01-05 15:22:04 +0100

    chore: test

---
+++ backend/src/test/java/hu/pte/mik/prog5/project/s3_manager/S3ManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package hu.pte.mik.prog5.project.s3_manager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class S3ManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
