7cdf41771337348cfa5b911b1e5e9acb386f9304
Adamkoro <adam.koronics@gmail.com>
2025-01-07 08:50:29 +0100

    fix: rename package

--- backend/src/main/java/hu/pte/mik/prog5/project/s3_manager/repository/S3EndpointRepository.java
+++ backend/src/main/java/hu/pte/mik/prog5/project/s3_manager/repository/S3EndpointRepository.java
@@ -1,11 +1,10 @@
-package hu.pte.mik.prog5.project.s3_manager.Repository;
+package hu.pte.mik.prog5.project.s3_manager.repository;
 
 import hu.pte.mik.prog5.project.s3_manager.entity.S3Endpoint;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
-import java.util.Optional;
 
 @Repository
 public interface S3EndpointRepository extends JpaRepository<S3Endpoint, Long> {
